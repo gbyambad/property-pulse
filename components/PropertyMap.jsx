df2e08adc15cacc313421285bd07a7d1b7852493
byambadorj2 <byambadorj993@gmail.com>
2025-01-29 15:40:01 +0800

    Geocoding & Mapbox Map

---
+++ components/PropertyMap.jsx
@@ -0,0 +1,70 @@
+"use client";
+import { useEffect, useState } from "react";
+import "mapbox-gl/dist/mapbox-gl.css";
+import Map, { Marker } from "react-map-gl";
+import { setDefaults, fromAddress } from "react-geocode";
+import Spinner from "@/components/Spinner";
+import Image from "next/image";
+import pin from "@/assets/images/pin.svg";
+import { set } from "mongoose";
+
+const PropertyMap = ({ property }) => {
+  const [lat, setLat] = useState(null);
+  const [lng, setLng] = useState(null);
+
+  const [viewport, setViewport] = useState({
+    latitude: 0,
+    longitude: 0,
+    zoom: 12,
+    width: "100%",
+    height: "500px",
+  });
+
+  const [loading, setLoading] = useState(true);
+
+  setDefaults({
+    key: process.env.NEXT_PUBLIC_GOOGLE_GEOCODING_API_KEY,
+    language: "en",
+    region: "us",
+  });
+
+  useEffect(() => {
+    const fetchCoordinates = async () => {
+      const res = await fromAddress(
+        `${property.location.street}, ${property.location.city}, ${property.location.state}, ${property.location.zipcode}`
+      );
+      console.log(res);
+
+      const { lat, lng } = res.results[0].geometry.location;
+      setLat(lat);
+      setLng(lng);
+      setViewport({ ...viewport, latitude: lat, longitude: lng });
+      setLoading(false);
+    };
+
+    fetchCoordinates();
+  }, []);
+
+  if (loading) return <Spinner loading={loading} />;
+  return (
+    !loading && (
+      <Map
+        mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
+        mapLib={import("mapbox-gl")}
+        initialViewState={{
+          latitude: lat,
+          longitude: lng,
+          zoom: 15,
+        }}
+        style={{ width: "100%", height: "500px" }}
+        mapStyle="mapbox:// styles/mapbox/streets-v9"
+      >
+        <Marker latitude={lat} longitude={lng} anchor="bottom">
+          <Image src={pin} alt="location" width={40} height={40} />
+        </Marker>
+      </Map>
+    )
+  );
+};
+
+export default PropertyMap;
